5fd70a089b033a1d05cf9046fd4cb73871416211
Mrr-kat <shidoxxthoka@gmail.com>
2025-12-04 08:16:38 -0500

    u M.0.1.py

--- M.0.1.py
+++ M.0.1.py
@@ -541,6 +541,5 @@ def liberar_recursos():
 
 # Main
 if __name__ == "__main__":
-    threading.Thread(target=escucha_pasiva, daemon=True).start()
-    import uvicorn
-    uvicorn.run(app_mount, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app_mount, host="0.0.0.0", port=port)
